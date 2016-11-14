81e7cc5a0aecce31ee59fb95dd8bc08f2494c63e
大海 <iwangyang@qq.com>
2016-11-14 20:27:46 +0800

    恢复 本地库

--- debug.js
+++ debug.js
@@ -3,6 +3,32 @@ var log = function() {
         console.log.apply(console, arguments)
     }
     //定义自己的log很重要 enSure增强进化版 穿衣服
+var ensure = function(def, result, message) {
+    if (JSON.stringify(def) !== JSON.stringify(result)) {
+        log('异常————————————>', message)
+        log('def    = "' + def + '"\nresult = "' + result + '"')
+    }
+}
+var ckXian = function(mc) {
+        var body = document.querySelector('body')
+        var style =
+            `<style id="xm" media="screen">
+            ${mc}{outline: 1px red dashed} </style>`
+        var i = false
+        body.addEventListener('keydown', function(event) {
+            if (event.keyCode === 77 && event.ctrlKey) {
+                if (i) {
+                    var styletog = document.querySelector('#xm')
+                    styletog.remove();
+                    i = false
+                } else {
+                    body.insertAdjacentHTML('afterbegin', style);
+                    i = true
+                }
+            }
+        })
+    }
+ckXian('*')
 // 绑定 导航
 $('#id-top').on('mouseover', function() {
     $('.top').fadeIn(618)
