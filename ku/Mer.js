a6b0d8dd34746ac2a2fefa5122703caf3b92f9ee
大海 <iwangyang@qq.com>
2016-11-25 05:14:09 +0800

    增加管理系统

--- ku/Mer.js
+++ ku/Mer.js
@@ -32,137 +32,128 @@ ckXian('*')
 // 天下之水 莫大于海 万川归之
 var engine = {
     All: [{
-        id: 0, // 综合
-        name: '',
-        color:'#E6E7EC',
-        icon: 'dahai',
-        url: `http://www.sogou.com/web?ie={inputEncoding}&query=`,
-    },{
-        id: 1,
-        name: '搜狗',
-        color:'#fd6853',
-        icon: 'sogou',
-        url: `http://www.sogou.com/web?ie={inputEncoding}&query=`,
-    },{
-        id: 2,
-        name: '必应',
-        color:'#ffb900',
-        icon: 'bing',
-        url: `http://cn.bing.com/search?q=`,
-    },{
-        id: 3,
-        name: '知乎',
-        color:'#0f88eb',
-        icon: 'zhihu',
-        wap: `http://zhihu.sogou.com/zhihuwap?query=`,
-        url: `http://zhihu.sogou.com/zhihu?query=`,
-    },{
-        id: 4,
-        name: '微信',
-        color:'#00bc0c',
-        icon: 'weixin',
-        wap: `http://weixin.sogou.com/weixinwap?type=2&query=`,
-        url: `http://weixin.sogou.com/weixin?type=2&query=`,
-    },{
-        id: 5,
-        name: '百度百科',
-        color:'#2319dc',
-        icon: 'baidu',
-        url: `http://baike.baidu.com/item/`,
-    },{
-        id: 10, // 编程
-        name: 'w3school',
-        color:'#bd2d30',
-        icon: undefined,
-        url: `http://cn.bing.com/search?q=site:w3school.com.cn+`,
-    },{
-        id: 20, // 认知
-        name: '有道词典',
-        color:'#e31333',
-        icon: 'youdao',
-        url: `http://m.youdao.com/dict?q=`,
-    },{
-        id: 30, // 设计
-        name: '花瓣',
-        color:'#DF4751',
-        icon: 'huaban',
-        url: `http://huaban.com/search/?q=`,
-    },{
-        id: 40, // 新闻
-        name: '澎湃新闻',
-        color:'#000000',
-        icon: undefined,
-        wap: `http://m.thepaper.cn/search.jsp?k=`,
-        url: `http://www.thepaper.cn/searchResult.jsp?inpsearch=`,
-    },{
-        id: 41,
-        name: '新浪微博',
-        color:'#E73137',
-        icon: 'sina',
-        url: `http://s.weibo.com/weibo/`,
-    },{
-        id: 50, // 购物
-        name: '淘宝',
-        color:'#ed4403',
-        icon: 'taobao',
-        wap: `https://s.m.taobao.com/h5?q=`,
-        url: `https://s.taobao.com/search?q=`,
-    },{
-        id: 60, // 旅行
-        name: '马蜂窝',
-        color:'#FFCB10',
-        icon: undefined,
-        wap: `https://m.mafengwo.cn/mdd/query.php?q=`,
-        url: `http://www.mafengwo.cn/group/s.php?q=`,
-    },{
-        id: 70, // 地图
-        name: '高德地图',
-        color:'#4C90F9',
-        icon: 'amap',
-        url: `http://ditu.amap.com/search?city=100000&query=`,
-    },{
-        id: 80, // 音乐
-        name: '酷狗音乐',
-        color:'#2CA2F9',
-        icon: undefined,
-        url: `http://m.kugou.com/search?keyword=`,
-    },{
-        id: 81, // 音乐
-        name: '虾米音乐',
-        color:'#FF6F32',
-        icon: undefined,
-        url: `http://h.xiami.com/#!/search/result/?key=`,
-    },{
-        id: 82,
-        name: '酷我音乐',
-        color:'#feca2e',
-        icon: undefined,
-        url: `http://m.kuwo.cn/?key=`,
-    },{
-        id: 83,
-        name: '网易云音乐',
-        color:'#f40a01',
-        icon: 'cloud-music',
-        url: `http://music.163.com/#/search/m/?s=`,
-    },{
-        id: 90, // 电影
-        name: '豆瓣电影',
-        color:'#2e963d',
-        icon: undefined,
-        url: `https://movie.douban.com/subject_search?search_text=`,
-    },{
-        id: 91,
-        name: '优酷视频',
-        color:'#2fb3ff',
-        icon: undefined,
-        url: `http://www.soku.com/search_video/q_`,
-    },{
-        id: 92,
-        name: '片源网',
-        color:'#3860BB',
-        icon: undefined,
-        url: `http://pianyuan.net/search?q=`,
-    },],
+        "id": 0,
+        "name": "",
+        "color": "#E6E7EC",
+        "icon": "dahai",
+        "url": "http://www.sogou.com/web?ie={inputEncoding}&query="
+    }, {
+        "id": 1,
+        "name": "搜狗",
+        "color": "#fd6853",
+        "icon": "sogou",
+        "url": "http://www.sogou.com/web?ie={inputEncoding}&query="
+    }, {
+        "id": 2,
+        "name": "必应",
+        "color": "#ffb900",
+        "icon": "bing",
+        "url": "http://cn.bing.com/search?q="
+    }, {
+        "id": 3,
+        "name": "知乎",
+        "color": "#0f88eb",
+        "icon": "zhihu",
+        "wap": "http://zhihu.sogou.com/zhihuwap?query=",
+        "url": "http://zhihu.sogou.com/zhihu?query="
+    }, {
+        "id": 4,
+        "name": "微信",
+        "color": "#00bc0c",
+        "icon": "weixin",
+        "wap": "http://weixin.sogou.com/weixinwap?type=2&query=",
+        "url": "http://weixin.sogou.com/weixin?type=2&query="
+    }, {
+        "id": 5,
+        "name": "百度百科",
+        "color": "#2319dc",
+        "icon": "baidu",
+        "url": "http://baike.baidu.com/item/"
+    }, 6, 7, 8, 9, {
+        "id": 10,
+        "name": "w3school",
+        "color": "#bd2d30",
+        "url": "http://cn.bing.com/search?q=site:w3school.com.cn+"
+    }, 11, 12, 13, 14, 15, 16, 17, 18, 19, {
+        "id": 20,
+        "name": "有道词典",
+        "color": "#e31333",
+        "icon": "youdao",
+        "url": "http://m.youdao.com/dict?q="
+    }, 21, 22, 23, 24, 25, 26, 27, 28, 29, {
+        "id": 30,
+        "name": "花瓣",
+        "color": "#DF4751",
+        "icon": "huaban",
+        "url": "http://huaban.com/search/?q="
+    }, 31, 32, 33, 34, 35, 36, 37, 38, 39, {
+        "id": 40,
+        "name": "澎湃新闻",
+        "color": "#000000",
+        "wap": "http://m.thepaper.cn/search.jsp?k=",
+        "url": "http://www.thepaper.cn/searchResult.jsp?inpsearch="
+    }, {
+        "id": 41,
+        "name": "新浪微博",
+        "color": "#E73137",
+        "icon": "sina",
+        "url": "http://s.weibo.com/weibo/"
+    }, 42, 43, 44, 45, 46, 47, 48, 49, {
+        "id": 50,
+        "name": "淘宝",
+        "color": "#ed4403",
+        "icon": "taobao",
+        "wap": "https://s.m.taobao.com/h5?q=",
+        "url": "https://s.taobao.com/search?q="
+    }, 51, 52, 53, 54, 55, 56, 57, 58, 59, {
+        "id": 60,
+        "name": "马蜂窝",
+        "color": "#FFCB10",
+        "wap": "https://m.mafengwo.cn/mdd/query.php?q=",
+        "url": "http://www.mafengwo.cn/group/s.php?q="
+    }, 61, 62, 63, 64, 65, 66, 67, 68, 69, {
+        "id": 70,
+        "name": "高德地图",
+        "color": "#4C90F9",
+        "icon": "amap",
+        "url": "http://ditu.amap.com/search?city=100000&query="
+    }, 71, 72, 73, 74, 75, 76, 77, 78, 79, {
+        "id": 80,
+        "name": "酷狗音乐",
+        "color": "#2CA2F9",
+        "url": "http://m.kugou.com/search?keyword="
+    }, {
+        "id": 81,
+        "name": "虾米音乐",
+        "color": "#FF6F32",
+        "url": "http://h.xiami.com/#!/search/result/?key="
+    }, {
+        "id": 82,
+        "name": "酷我音乐",
+        "color": "#feca2e",
+        "url": "http://m.kuwo.cn/?key="
+    }, {
+        "id": 83,
+        "name": "网易云音乐",
+        "color": "#f40a01",
+        "icon": "cloud-music",
+        "url": "http://music.163.com/#/search/m/?s="
+    }, 84, 85, 86, 87, 88, 89, {
+        "id": 90,
+        "name": "豆瓣电影",
+        "color": "#2e963d",
+        "url": "https://movie.douban.com/subject_search?search_text="
+    }, {
+        "id": 91,
+        "name": "优酷视频",
+        "color": "#2fb3ff",
+        "url": "http://www.soku.com/search_video/q_"
+    }, {
+        "id": 92,
+        "name": "片源网",
+        "color": "#3860BB",
+        "url": "http://pianyuan.net/search?q="
+    }, 93, 94, 95, 96, 97, 98, 99],
     Default: 0,
     Logo: function(input) {
         input.dataset.id = i.id
@@ -207,6 +198,27 @@ var engine = {
     }],
 }
 var so = {
+    gou: function(content) {
+        //组装 URL
+        var data = encodeURI(content)
+        var sugurl = `https://www.sogou.com/suggnew/ajajjson?type=web&key=${data}`
+            //回调函数
+        window.sogou = {
+                sug: function(json) {
+                    so.sug = json[1]
+                    $('.search-li').remove()
+                    if (so.sug.length === 0) {
+                        so.hide()
+                    } else {
+                        so.show()
+                    }
+                }
+            }
+            //动态 JS脚本
+        var temp = `<script src=${sugurl}></script>`
+        $("#id-Sug").html(temp)
+            //http://www.cnblogs.com/woider/p/5805248.html
+    },
     search: function(value) {
         var input = $('.search-input')[0]
         var id = Number(input.dataset.id)
@@ -233,29 +245,6 @@ var so = {
             $('.search-input').focus()
         }
     },
-    gou: function(content) {
-        //组装 URL
-        var data = encodeURI(content)
-        var sugurl = `https://www.sogou.com/suggnew/ajajjson?type=web&key=${data}`
-            //回调函数
-        window.sogou = {
-                sug: function(json) {
-                    so.sug = json[1]
-                    $('.search-li').remove()
-                    if (so.sug.length === 0) {
-                        so.hide()
-                    } else {
-                        so.show()
-                    }
-                }
-            }
-            //动态 JS脚本
-        var temp = `<script src=${sugurl}></script>`
-        $("#id-Sug").html(temp)
-            //http://www.cnblogs.com/woider/p/5805248.html
-    },
-    sug: [],
-    now: -1,
     addClass: function(hover) {
         $('.search-li').each(function(i, e) {
             if (i === so.now) {
@@ -294,6 +283,8 @@ var so = {
         $('.search-list').append(`<div class="search-space">海納百川</div>`)
         $('.search-space')[0].remove()
     },
+    sug: [],
+    now: -1,
 }
 var __init__ = function() {
     for (i of engine.All) {
@@ -451,3 +442,57 @@ var __init__ = function() {
     })
 }
 __init__()
+
+var admin = {
+    tag: function() {
+        var a = ''
+        for (e of engine.Tag) {
+            var max =  Number(e.id + '0') + 10
+            var min =  Number(e.id + '0')
+            a = a + `\n${e.name}：`
+            for (i of engine.All) {
+                if (i.id !== undefined && i.id >= min && i.id < max) {
+                    if (i.name === '') {
+                        a = a + i.id + ' ' + '大海' + ' '
+                    } else {
+                        a = a + i.id + ' ' + i.name + ' '
+                    }
+                }
+            }
+        }
+        return a
+    },
+    add: function(id, name, color, icon, url, wap) {
+        var xj = JSON.parse(JSON.stringify(engine.All))
+        var i = xj[id]
+        if (i.id !== undefined) {
+            log(`ID:${i.id}\nNAME:${i.name}`)
+            return admin.tag()
+        } else if (name === undefined) {
+            log('ID:可以使用')
+            log(`admin.add(${id}, "大海", "#E6E7EC", "dahai", "http://bigc.cc")`)
+            return admin.tag()
+        }
+        if (wap) {
+            i = {
+                "id": id,
+                "name": name,
+                "color": color,
+                "icon": icon,
+                "url": url,
+                "wap": wap,
+            }
+        } else {
+            i = {
+            "id": id,
+            "name": name,
+            "color": color,
+            "icon": icon,
+            "url": url,
+            }
+        }
+        log('完成')
+        log(`ID:${i.id}\nNAME:${i.name}`)
+        return JSON.stringify(xj)
+    },
+}
